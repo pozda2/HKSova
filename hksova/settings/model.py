b858f31b40959f59ab59579a38704a32dd40410a
Gimli2 <gimli2@gmail.com>
2022-07-18 21:48:39 +0200

    code cleanup - setting module

--- hksova/settings/model.py
+++ hksova/settings/model.py
@@ -1,29 +1,29 @@
-import sys
-from flask import current_app
-from datetime import datetime
+import base64
+from datetime import datetime, timedelta
 from dateutil import parser
-from datetime import timedelta
+from flask import current_app
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import base64
 
 
 def get_settings_year(year, param):
     cursor = current_app.mysql.connection.cursor()
     cursor.execute('''SELECT value FROM setting where idYear=%s and param=%s''', [year['year'], param])
     data = cursor.fetchall()
-    if not data: return None
-    value=data[0]['value']
-    
+    if not data:
+        return None
+    value = data[0]['value']
+
     if param == "email-smtp-password":
         kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=b'246', iterations=390000, )
         key = base64.urlsafe_b64encode(kdf.derive(str.encode(current_app.config['SECRET_PEPPER'])))
         cipher_suite = Fernet(key)
-        encoded_text=str.encode(data[0]['value'])
+        encoded_text = str.encode(data[0]['value'])
         decoded_text = cipher_suite.decrypt(encoded_text)
-        value=decoded_text.decode("utf-8")
+        value = decoded_text.decode("utf-8")
     return value
+
 
 def get_settings_global(param):
     cursor = current_app.mysql.connection.cursor()
@@ -31,36 +31,40 @@ def get_settings_global(param):
     data = cursor.fetchall()
     return data[0]['value']
 
+
 def is_registration_open(year):
     reg_from = parser.parse(get_settings_year(year, 'reg-from'))
     reg_to = parser.parse(get_settings_year(year, 'reg-to'))
-    today=datetime.now() 
+    today = datetime.now()
 
-    if (today > reg_from and today < reg_to + timedelta(days=1)):
-        return True
-    else:
-        return False
+    return bool(today > reg_from and (today < reg_to + timedelta(days=1)))
 
-def get_registration_from(year):    
+
+def get_registration_from(year):
     return parser.parse(get_settings_year(year, "reg-from")).strftime("%-d. %-m. %Y")
+
 
 def get_registration_to(year):
     return parser.parse(get_settings_year(year, "reg-to")).strftime("%-d. %-m. %Y")
 
+
 def get_min_players(year):
     return int(get_settings_year(year, "min-players"))
+
 
 def get_max_players(year):
     return int(get_settings_year(year, "max-players"))
 
+
 def get_max_teams(year):
     return int(get_settings_year(year, "max-teams"))
 
+
 def get_payment_information(year):
-    data={}
-    data['account']=get_settings_year(year, 'payment-account')
-    data['iban']=get_settings_year(year, "payment-iban")
-    data['price']=get_settings_year(year, 'payment-price')
-    data['unit']=get_settings_year(year, 'payment-unit')
-    data['to']=parser.parse(get_settings_year(year, "payment-to")).strftime("%-d. %-m. %Y")
+    data = {}
+    data['account'] = get_settings_year(year, 'payment-account')
+    data['iban'] = get_settings_year(year, "payment-iban")
+    data['price'] = get_settings_year(year, 'payment-price')
+    data['unit'] = get_settings_year(year, 'payment-unit')
+    data['to'] = parser.parse(get_settings_year(year, "payment-to")).strftime("%-d. %-m. %Y")
     return data
